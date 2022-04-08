c332efe460e6b0915fddb53132da42b03c1690ce
JAQUELINE JUAREZ SILVA <jaquelinejuarez.silva03@gmail.com>
2022-04-08 12:59:46 -0500

    Ejemplo de manejo de variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 
+//ECMAscript 5
+var textovar = "Texto var1";
 
-function App() {
+//ECMAScript 6
+let textolet = "Texto let 1";
+const textoconst = "Texto const 1";
+
+textovar = "texto var 2";
+textolet = "texto let 2";
+// textoconst "texto var 2";
+
+
+var textovar = "texto var 3";
+//let textolet = "texto let 3";
+//const textoconst = "texto const 3";
+
+if(true){
+var textovar=5;
+let textolet=10;
+// textoconst = 15;
+}
+
+const App= () => {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>Segundo Proyecto</h1>
-      </header>
+      <h1>Segundo Proyecto</h1>
+    <h1>{textovar}</h1>
+     <h1>{textolet}</h1>
+     <h1>{textoconst}</h1>
     </div>
   );
 }
-
 export default App;
