56707ae5ee8008465c27b4bd4304af7818058636
JAQUELINE JUAREZ SILVA <jaquelinejuarez.silva03@gmail.com>
2022-04-08 13:13:21 -0500

    Utilizando objetos

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
 
-//ECMAscript 5
-var textovar = "Texto var1";
-
-//ECMAScript 6
-let textolet = "Texto let 1";
-const textoconst = "Texto const 1";
-
-textovar = "texto var 2";
-textolet = "texto let 2";
-// textoconst "texto var 2";
-
-
-var textovar = "texto var 3";
-//let textolet = "texto let 3";
-//const textoconst = "texto const 3";
-
-if(true){
-var textovar=5;
-let textolet=10;
-// textoconst = 15;
-}
-
 const App= () => {
-  return (
+  const miObjeto={
+    nombre:"Jaqueline",
+    ApPaterno:"Juarez",
+    ApMaterno:"Silva",
+    Edad:21,
+  };
+  const atributo="ApPaterno"
+  return(
     <div className="App">
-      <h1>Segundo Proyecto</h1>
-    <h1>{textovar}</h1>
-     <h1>{textolet}</h1>
-     <h1>{textoconst}</h1>
+      {/*ECMAScript 6*/}
+      <h1>{`Mi nombre es ${miObjeto.nombre} ${miObjeto.ApPaterno} ${miObjeto.ApMaterno} y tengo ${miObjeto.Edad} años`}</h1>
+     {/*ECMAScript 6*/}
+     <h2>{"Mi nombre es "+
+     miObjeto.nombre+" "+miObjeto.ApPaterno+" "+miObjeto.ApMaterno+" y tengo "+miObjeto.Edad+" años"}</h2>
     </div>
   );
 }
+
+
 export default App;
